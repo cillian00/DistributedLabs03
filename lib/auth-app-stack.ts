3e625e80410ef7bd4206aacaec9729af7bad79c5
cillian00 <20094972@mail.wit.ie>
2023-11-07 14:11:31 +0000

    Confirm signup resource.

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -68,6 +68,13 @@ export class AuthAppStack extends cdk.Stack {
         'signup.ts'
     );
 
+    this.addAuthRoute(
+        "confirm_signup",
+        "POST",
+        "ConfirmFn",
+        "confirm-signup.ts"
+    );
+
 
     const fn = new node.NodejsFunction(this, fnName, {
       ...commonFnProps,
